a2c29bade03077a0102a7edcfd626b0482f718f4
Lucy Linder <lucy.derlin@gmail.com>
2022-07-26 15:34:45 +0200

    fix: fix page title on signup and login (still using 780 words)

--- src/log_in.php
+++ src/log_in.php
@@ -14,7 +14,7 @@ if (!isset($_POST['submitok'])):
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <meta charset="utf-8">
-        <title>780 words</title>
+        <title>450 words</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <!--[if lt IE 9]>
         <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
